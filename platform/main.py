427b07f0e57b5bd1e3879d313a97d99167f06f41
Nandha Sundaravadivel <nandhasundaravadivel@lawn-128-61-4-15.lawn.gatech.edu>
2020-10-08 16:07:30 -0400

    Template added

---
+++ platform/main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET'])
+def hello_world():
+    return 'Welcome to BDBI Platform!'
+
+@app.route('/all', methods=['GET'])
+def returnAll():
+    #return all data that exists
+    return {}
---
+++ platform/req.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+import sys
+import os
+from IPython.display import display
+
+# Declare all variables as strings. Spaces must be replaced with '+', i.e., change 'John Smith' to 'John+Smith'.
+# Define the lat, long of the location and the year
+lat, lon, year = 33.2164, -97.1292, 2010
+# You must request an NSRDB api key from the link above
+api_key = 'gv55YTdtraJqabUYQaUcJJSXkzir9Mq5FvZGgLHs'
+# Set the attributes to extract (e.g., dhi, ghi, etc.), separated by commas.
+attributes = 'ghi,dhi,dni,wind_speed,air_temperature,solar_zenith_angle'
+# Choose year of data
+year = '2010'
+# Set leap year to true or false. True will return leap day data if present, false will not.
+leap_year = 'false'
+# Set time interval in minutes, i.e., '30' is half hour intervals. Valid intervals are 30 & 60.
+interval = '30'
+# Specify Coordinated Universal Time (UTC), 'true' will use UTC, 'false' will use the local time zone of the data.
+# NOTE: In order to use the NSRDB data in SAM, you must specify UTC as 'false'. SAM requires the data to be in the
+# local time zone.
+utc = 'false'
+# Your full name, use '+' instead of spaces.
+your_name = 'Nandha+Sundaravadivel'
+# Your reason for using the NSRDB.
+reason_for_use = 'testing'
+# Your affiliation
+your_affiliation = 'Georgia+Tech'
+# Your email address
+your_email = 'nsundara3@gatech.edu'
+# Please join our mailing list so we can keep you up-to-date on new developments.
+mailing_list = 'true'
+
+# Declare url string
+url = 'http://developer.nrel.gov/api/solar/nsrdb_psm3_download.csv?wkt=POINT({lon}%20{lat})&names={year}&leap_day={leap}&interval={interval}&utc={utc}&full_name={name}&email={email}&affiliation={affiliation}&mailing_list={mailing_list}&reason={reason}&api_key={api}&attributes={attr}'.format(
+    year=year, lat=lat, lon=lon, leap=leap_year, interval=interval, utc=utc, name=your_name, email=your_email, mailing_list=mailing_list, affiliation=your_affiliation, reason=reason_for_use, api=api_key, attr=attributes)
+# Return just the first 2 lines to get metadata:
+info = pd.read_csv(url, nrows=1)
+# See metadata for specified properties, e.g., timezone and elevation
+timezone, elevation = info['Local Time Zone'], info['Elevation']
