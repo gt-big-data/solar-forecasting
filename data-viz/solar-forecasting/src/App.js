e671dba91a17692a5a9d33c0c2e811e23a62f3ae
spatel776 <71471558+spatel776@users.noreply.github.com>
2020-11-14 17:48:06 -0500

    Add files via upload

--- data-viz/solar-forecasting/src/App.js
+++ data-viz/solar-forecasting/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import SolarTitle from './components/SolarTitle';
 import SolarMap from './components/SolarMap';
+import SolarGraph from './components/SolarGraph';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Switch>
         <Route path="/" exact component={SolarTitle} />
         <Route path="/solarmap" component={SolarMap}/>
+        <Route path="/solargraph" component={SolarGraph}/>
         </Switch>
       </div>
     </Router>
