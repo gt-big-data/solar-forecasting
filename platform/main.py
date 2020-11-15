c9caef590e5b045f97d003dac8dd4bc9d055853f
Nandha Sundaravadivel <nandhasundaravadivel@lawn-128-61-4-15.lawn.gatech.edu>
2020-11-15 16:58:11 -0500

    Condense code and add add request

--- platform/main.py
+++ platform/main.py
@@ -2,99 +2,35 @@ from flask import Flask
 from flask import request, send_file
 #json or geojason
 import pandas as pd
+import req
 app = Flask(__name__)
 app.config["DEBUG"] = True
-
-api_key = 'sAPoz5XEVqaCq30LmasEWKk0ZV81gi4Vlf4cwONW'
-leap_year = 'false'
-interval = '30'
-utc = 'false'
-your_name = 'Udit+Subramanya'
-reason_for_use = 'solar+forecasting'
-your_affiliation = 'georgia+tech'
-your_email = 'usubramanya3@gatech.edu'
-mailing_list = 'false'
-locations = {}
-locations['Butler Solar Farm'] = [32.55, -84.27]
-locations['Simon Solar Farm'] = [33.677, -83.676]
-locations['Meriweather Solar Farm'] = [32.965, -84.543]
-locations['Dublin Solar Farm'] = [32.52, -82.944]
 
 @app.route('/', methods = ['GET'])
 def hello_world():
     return request.args.get('key','')
-
-@app.route('/all', methods=['GET'])
-def return_all():
-    #return all data that exists
-    return {}     
-
+  
 @app.route('/data/csv', methods=['GET'])
 def csv_data():
-    attributes = request.args.get('attributes','')
-    year = request.args.get('year','')
-    location = request.args.get('location','')
-    lat, lon = locations[location]
-    url = ("https://developer.nrel.gov/api/solar/nsrdb_psm3_download.csv?wkt="
-        "POINT({lon}%20{lat})"
-        "&names={year}"
-        "&leap_day={leap}"
-        "&interval={interval}"
-        "&utc={utc}"
-        "&full_name={name}"
-        "&email={email}"
-        "&affiliation={affiliation}"
-        "&mailing_list={mailing_list}"
-        "&reason={reason}"
-        "&api_key={api_key}"
-        "&attributes={attr}").format(
-            year=year,
-            lat=lat, lon=lon,
-            leap=leap_year,
-            interval=interval,
-            utc=utc,
-            name=your_name,
-            email=your_email,
-            mailing_list=mailing_list,
-            affiliation=your_affiliation,
-            reason=reason_for_use,
-            api_key=api_key,
-            attr=attributes)
+    url = req.return_url(year = request.args.get('year',''),attributes = request.args.get('attributes',''), location = request.args.get('location',''))
     df = pd.read_csv(url, skiprows=2)
-    return df.to_csv()
+    return df.to_csv(), 200
 
 @app.route('/data/json', methods=['GET'])
 def json_data():
-    attributes = request.args.get('attributes','')
-    year = request.args.get('year','')
-    location = request.args.get('location','')
-    lat, lon = locations[location]
-    url = ("https://developer.nrel.gov/api/solar/nsrdb_psm3_download.csv?wkt="
-        "POINT({lon}%20{lat})"
-        "&names={year}"
-        "&leap_day={leap}"
-        "&interval={interval}"
-        "&utc={utc}"
-        "&full_name={name}"
-        "&email={email}"
-        "&affiliation={affiliation}"
-        "&mailing_list={mailing_list}"
-        "&reason={reason}"
-        "&api_key={api_key}"
-        "&attributes={attr}").format(
-            year=year,
-            lat=lat, lon=lon,
-            leap=leap_year,
-            interval=interval,
-            utc=utc,
-            name=your_name,
-            email=your_email,
-            mailing_list=mailing_list,
-            affiliation=your_affiliation,
-            reason=reason_for_use,
-            api_key=api_key,
-            attr=attributes)
+    url = req.return_url(year = request.args.get('year',''),attributes = request.args.get('attributes',''), location = request.args.get('location',''))
     df = pd.read_csv(url, skiprows=2)
-    return df.to_json()
+    return df.to_json(), 200
+
+@app.route('/add', methods = ['PUT'])
+def add():
+    csv_string = request.form["data"]
+    #Add to database
+    
+    #Check if item is in database
+    if(true):
+        return "Success", 200
+    else:
+        return "Error", 400
 
 app.run()
--- platform/req.py
+++ platform/req.py
@@ -1,55 +1,48 @@
 import pandas as pd
 import numpy as np
-import sys
-import os
-from IPython.display import display
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import seaborn as sns
 
-
-# Declare all variables as strings. Spaces must be replaced with '+', i.e., change 'John Smith' to 'John+Smith'.
-# Define the lat, long of the location and the year
-lat, lon = 32.55, -84.27
-# You must request an NSRDB api key from the link above
-api_key = 'gv55YTdtraJqabUYQaUcJJSXkzir9Mq5FvZGgLHs'
-# Set the attributes to extract (e.g., dhi, ghi, etc.), separated by commas.
-attributes = 'ghi,clearsky_ghi'
-year = '1998'
-# Set leap year to true or false. True will return leap day data if present, false will not.
+api_key = 'sAPoz5XEVqaCq30LmasEWKk0ZV81gi4Vlf4cwONW'
 leap_year = 'false'
-# Set time interval in minutes, i.e., '30' is half hour intervals. Valid intervals are 30 & 60.
 interval = '30'
-# Specify Coordinated Universal Time (UTC), 'true' will use UTC, 'false' will use the local time zone of the data.
-# NOTE: In order to use the NSRDB data in SAM, you must specify UTC as 'false'. SAM requires the data to be in the
-# local time zone.
 utc = 'false'
-# Your full name, use '+' instead of spaces.
-your_name = 'Nandha+Sundaravadivel'
-# Your reason for using the NSRDB.
-reason_for_use = 'testing'
-# Your affiliation
-your_affiliation = 'Georgia+Tech'
-# Your email address
-your_email = 'nsundara3@gatech.edu'
-# Please join our mailing list so we can keep you up-to-date on new developments.
-mailing_list = 'true'
+your_name = 'Udit+Subramanya'
+reason_for_use = 'solar+forecasting'
+your_affiliation = 'georgia+tech'
+your_email = 'usubramanya3@gatech.edu'
+mailing_list = 'false'
 
-# Declare url string
-url = 'https://developer.nrel.gov/api/solar/nsrdb_psm3_download.csv?wkt=POINT({lon}%20{lat})&names={year}&leap_day={leap}&interval={interval}&utc={utc}&full_name={name}&email={email}&affiliation={affiliation}&mailing_list={mailing_list}&reason={reason}&api_key={api}&attributes={attr}'.format(
-    year=year, lat=lat, lon=lon, leap=leap_year, interval=interval, utc=utc, name=your_name, email=your_email, mailing_list=mailing_list, affiliation=your_affiliation, reason=reason_for_use, api=api_key, attr=attributes)
-# Return just the first 2 lines to get metadata:
-info = pd.read_csv(url, low_memory=False)
-# See metadata for specified properties, e.g., timezone and elevation
-timezone, elevation = info['Local Time Zone'], info['Elevation']
-temperature = info['Latitude']
-new_headers = info.iloc[1]
-info = info[2:]
-info.columns = new_headers
-info = info.dropna(how='all',axis=1)
-print((info[40:]))
+locations = {}
+locations['Butler Solar Farm'] = [32.55, -84.27]
+locations['Simon Solar Farm'] = [33.677, -83.676]
+locations['Meriweather Solar Farm'] = [32.965, -84.543]
+locations['Dublin Solar Farm'] = [32.52, -82.944]
 
 
-
-
+def return_url(year, attributes, location):
+    lat, lon = locations[location]
+    url = ("https://developer.nrel.gov/api/solar/nsrdb_psm3_download.csv?wkt="
+           "POINT({lon}%20{lat})"
+           "&names={year}"
+           "&leap_day={leap}"
+           "&interval={interval}"
+           "&utc={utc}"
+           "&full_name={name}"
+           "&email={email}"
+           "&affiliation={affiliation}"
+           "&mailing_list={mailing_list}"
+           "&reason={reason}"
+           "&api_key={api_key}"
+           "&attributes={attr}").format(
+        year=year,
+        lat=lat, lon=lon,
+        leap=leap_year,
+        interval=interval,
+        utc=utc,
+        name=your_name,
+        email=your_email,
+        mailing_list=mailing_list,
+        affiliation=your_affiliation,
+        reason=reason_for_use,
+        api_key=api_key,
+        attr=attributes)
+    return url
